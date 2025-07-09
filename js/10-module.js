ce0dd49f737d274ba8ca260434bcccbaf2d67b2c
Lyubov9302 <lyubovyurynets@gmail.com>
2025-07-09 18:41:16 +0300

    +++

--- js/10-module.js
+++ js/10-module.js
@@ -65,3 +65,91 @@
 //     clearInterval(timerId);
 // }, 5000)
 
+// 7 -- показуємо та ховаємо, додаючи та видаляючи клас is-visible
+// - ховаємо через певний час
+// - ховаємо при кліці
+// - не забуваємо чистити таймер
+
+// const NOTIFICATION_DELAY = 3000;
+
+// const notification = document.querySelector(".js-alert");
+
+// let timerId = null;
+
+// notification.addEventListener('click', onNotificationClick);
+
+// showNotification();
+
+
+
+// function onNotificationClick() {
+// hideNotification();
+// clearTimeout(timerId);
+// }
+
+
+
+// function showNotification() {
+//     notification.classList.add("is-visible");
+
+//     timerId = setTimeout(() => {
+//    console.log("hidden");
+
+//        hideNotofication();
+//     }, NOTIFICATION_DELAY)
+// }
+
+
+// function hideNotification() {
+//     notification.classList.remove("is-visible");
+// }
+
+// 8 -- 
+// - створення
+// - unix - час
+// - методи
+// - різниця часу
+// - Date.now()
+
+const currentDate = new Date();
+
+// const day = currentDate.getDay();
+// const date = currentDate.getDate();
+// const month = currentDate.getMonth();
+// const year = currentDate.getFullYear();
+
+// console.log(day);
+// console.log(date);
+// console.log(month);
+// console.log(year);
+// console.log(currentDate);
+
+// можна перезаписати
+// currentDate.setFullYear(2007);
+
+// час який пройшов з 1.1.1970 на даний час
+// console.dir(currentDate.getTime());
+
+// console.dir(currentDate);
+
+
+// отримати кількість мілісекунд іншим способом:
+
+// console.log(Date.now());
+
+
+// - рахуємо різницю часу
+
+// const startTime = Date.now();
+
+// setTimeout(() => {
+//     const currentTime = Date.now();
+
+//     console.log(currentTime - startTime);
+
+// }, 2000);
+
+
+// 9 - написати клас timer який буде 
+// запускати та зупиняти відлік часу
+
