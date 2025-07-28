6d6d2d22a5b2428326eddabd3fa0eeaa11bc4826
Lyubov9302 <lyubovyurynets@gmail.com>
2025-07-28 18:56:57 +0300

    +

--- js/1-module.js
+++ js/1-module.js
@@ -1,3 +1,32 @@
+// 1 - a classwork
+// напиши функцію calcBMI(weight, height) яка розрах 
+// татповерт індекс маси - тіла людини. Для цього необх 
+// розділити вагу на квадрат висоти.
+
+
+// Вага та висота будуть спеціально передані як рядки,
+//  не цілі числа. Можуть будуть задані у вагляді 24,7 або 24.7
+//  Тобто як роздільник дробової частини може бути кома
+
+//  Індекс маси тіла необхідно округлити до однієї цифри після коми
+
+
+
+
+
+// function calcBMI(weight, height) {
+//     let numWeight = weight.replace("," , ".");
+//     numWeight = Number.parseFloat(numWeight);
+//     const numHeight = Number.parseFloat(height);
+
+
+//     const bmi = numWeight / (numHeight ** 2);
+//     return bmi.toFixed(1);
+// }
+
+
+// const bmi = calcBMI("88,3", "1.75");
+// console.log(bmi);
 
 // // Homework - task1
 // function makeTransaction(quantity, pricePerDroid) {
