129781ea300d0a6c61dff76d51fa73e370dbcba4
Lyubov9302 <lyubovyurynets@gmail.com>
2025-08-02 12:07:58 +0300

    +

--- js/7-module.js
+++ js/7-module.js
@@ -105,7 +105,7 @@
 // зчитування 
 // запис
 
-const titleEl = document.querySelector('.title');
+// const titleEl = document.querySelector('.title');
 
 // console.log(titleEl.innerHTML);
 
@@ -116,8 +116,93 @@ const titleEl = document.querySelector('.title');
 
 // 6 - вставка розмітки за допомогою insertAdjacentHTML
 
-titleEl.insertAdjacentHTML("beforeend", ' <a href="">Title</a>');
+// titleEl.insertAdjacentHTML("beforeend", ' <a href="">Title</a>');
+
+// 7 - подія click
+
+// Натискаючи на Click me змусь червоний 
+// квадратик зміщуватись на 50px по діагоналі
+
+
+// const btn = document.querySelector(".js-click");
+
+// const box = document.querySelector(".js-box");
+
+// btn.addEventListener("click", handleClick);
+// box.addEventListener("click", handleClick);
+ 
+// let step = 0;
+
+
+// function handleClick() {
+//     step += 50;
+//     box.style.marginTop = `${step}px`;
+//     box.style.marginLeft = `${step}px`;
+// }
+
+// 8  - подія input
+// подія blur
+
+// Виводь в консоль все, що користувач вводить в input
+
+
+// checkbox
+
+
+//  const jsCheckbox = document.querySelector(".js-checkbox");
+
+//  jsCheckbox.addEventListener("change", () => {
+//        console.log("ok");
+
+//  })
+
+// input 
+
+// const userName = document.querySelector(".js-user-name");
+
+// userName.addEventListener("input", (event) => {
+//     console.log(event.target.value);
+
+// })
+
+
+// 9 - користувач вводить в input своє імя після
+// втрати фокусу отримує alert з повідомленням привітання
 
 
 
+// const userName = document.querySelector(".js-user-name");
 
+// userName.addEventListener("blur", (event) => {
+//     const name = event.target.value;
+
+//     alert(`Hello ${name}`)
+// })
+
+// 10 - подія submit 
+// Дії браузера за замовчуванням
+// Властивість elements 
+
+// Оброби форму та збери відгук користувача в обєкт 
+
+// const form = document.querySelector(".js-form");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+// event.preventDefault();
+
+
+// const elements = event.target.elements;
+
+
+// const info = {
+//     email: elements.email.value,
+//     password: elements.password.value,
+//     comment: elements.comment.value
+// }
+
+// console.log(info);
+// event.target.reset();
+
+// }
